62f2a7fac66589a6282b0516c6f762c59cae409d
Arnout Kazemier <info@3rd-Eden.com>
2014-09-25 10:36:07 +0200

    [example] Show how you can create custom instances

--- example.js
+++ example.js
@@ -1,5 +1,21 @@
 'use strict';
 
-var Exception = require('./').listen();
+var Exception = require('./')
+  , path = require('path');
 
+//
+// Create a custom instance with a different path to store the dumps.
+//
+var Example = Exception.extend({
+  directory: path.join(__dirname, 'custom-path')
+});
+
+//
+// Add the default uncaught Exception listeners.
+//
+Example.listen();
+
+//
+// BOOM. DED.
+//
 throw new Error('fucked');
